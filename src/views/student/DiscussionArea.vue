c16e12dc4c99fd0148a5b5add0ff8717e81c639c
191250059 <191250059@nju.edu.cn>
2021-07-07 10:21:41 +0800

    feat: 讨论区跳转

--- src/views/student/DiscussionArea.vue
+++ src/views/student/DiscussionArea.vue
@@ -1,10 +1,18 @@
 <template>
-
+  <v-col>
+    <post-item
+      title="this is a post"
+      user-name="张三"
+    >
+    </post-item>
+  </v-col>
 </template>
 
 <script>
+import PostItem from "@/components/PostItem";
 export default {
-  name: "DiscussionArea"
+  name: "DiscussionArea",
+  components: { PostItem }
 };
 </script>
 
